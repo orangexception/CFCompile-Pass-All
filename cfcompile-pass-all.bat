e88f61d0ad39894fdf1c3f21b0416a045c35700c
Bradley Moore <bwmoore@orangexception.com>
2012-10-22 10:11:53 -0500

    Update cfcompile-pass-all.bat
    I revised and simplified the process. The environment variable JAVA_HOME is now required for Windows. findjava.bat and findjava.sh are not reliably installed with ColdFusion and I don't want to worry about license concerns.

--- cfcompile-pass-all.bat
+++ cfcompile-pass-all.bat
@@ -1,3 +1,8 @@
+@rem  Bradley Wayne Moore
+@rem	bwmoore@orangexception.com
+@rem	orangexception.com
+@rem	@orangexception
+
 @echo	off
 echo	".,;:=/|\=:;,."
 echo	"  cfcompile  "
@@ -11,6 +16,11 @@ echo	".,;:=/|\=:;,."
 @rem	4.	WWWROOT
 @rem	5.	APP
 @rem	6.	APP_COMPILED
+@rem	7.	JAVA_HOME
+echo	".,;:=/|\=:;,."
+echo	"  Arguments  "
+echo	".,;:=/|\=:;,."
+echo	".,;:=/|\=:;,."
 
 set	CFUSION_HOME=%1
 set	J2EEJAR=%2
@@ -18,6 +28,7 @@ set	WEBINF=%3
 set	WWWROOT=%4
 set	APP=%5
 set	APP_COMPILED=%6
+set	JAVA_HOME=%7
 
 echo	"CFUSION_HOME: %CFUSION_HOME%"
 echo	"J2EEJAR: %J2EEJAR%"
@@ -25,19 +36,15 @@ echo	"WEBINF: %WEBINF%"
 echo	"WWWROOT: %WWWROOT%"
 echo	"APP: %APP%"
 echo	"APP_COMPILED: %APP_COMPILED%"
-
-@rem	Find Java
-echo	".,;:=/|\=:;,."
-echo	"  Find Java  "
-echo	".,;:=/|\=:;,."
-echo	".,;:=/|\=:;,."
-CALL %CFUSION_HOME%\bin\findjava.bat
+echo	"JAVA_HOME: %JAVA_HOME%"
 
 @rem	Compile ColdFusion into Java ByteCode
 echo	".,;:=/|\=:;,."
 echo	"   Compile   "
 echo	".,;:=/|\=:;,."
 echo	".,;:=/|\=:;,."
+
+set	JAVACMD=%JAVA_HOME%
 %JAVACMD% -cp "%J2EEJAR%;%WEBINF%\lib\cfmx_bootstrap.jar;%WEBINF%\lib\cfx.jar" -Dcoldfusion.classPath=%CFUSION_HOME%/lib/updates,%CFUSION_HOME%/lib -Dcoldfusion.libPath=%CFUSION_HOME%/lib coldfusion.tools.CommandLineInvoker Compiler -webinf %WEBINF% -webroot %WWWROOT% -cfroot %CFUSION_HOME% -d -srcdir %APP% -deploydir %APP_COMPILED%
 
 echo	".,;:=/|\=:;,."
